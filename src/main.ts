044e1ace3b600d417263b591e1a071538f95a952
Muhamad Umer <umerdev228@gmail.com>
2023-05-15 20:00:02 +0500

    check in main.ts for font and awsome family

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,34 @@ import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+<!--    <a href="https://vitejs.dev" target="_blank">-->
+<!--      <img src="${viteLogo}" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://www.typescriptlang.org/" target="_blank">-->
+<!--      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />-->
+<!--    </a>-->
+<!--    <h1>Vite + TypeScript</h1>-->
+<!--    <div class="card">-->
+<!--      <button id="counter" type="button"></button>-->
+<!--    </div>-->
+<!--    <p class="read-the-docs">-->
+<!--      Click on the Vite and TypeScript logos to learn more-->
+<!--    </p>-->
+        <div id="app"></div>
+    <!-- solid style -->
+    <i class="fa-solid fa-user"></i>
+
+    <!-- regular style -->
+    <i class="fa-regular fa-user"></i>
+
+    <!-- all new sharp solid style -->
+    <i class="fa-sharp fa-solid fa-user"></i>
+
+    <!-- all new sharp regular style -->
+    <i class="fa-sharp fa-regular fa-user"></i>
+    <!--brand icon-->
+    <i class="fa-brands fa-github-square"></i>
+    <h1>Hello World 123 </h1>
   </div>
 `
 
